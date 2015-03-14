36852f5d79fd5a747a0fdac30985cc93dcd5fba0
Richard Ramsden <richard@rramsden.ca>
2015-03-14 13:47:48 +0900

    Fix test

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -3,12 +3,20 @@
 var helper = require('./helper');
 var expect = require('chai').expect;
 var Baragon = require('../lib/baragon');
+var fs = require('fs');
 
 describe('Generator module', function() {
   var schema = helper.loadSchema(__dirname + "/fixtures/schema.json");
 
   it("generates a client", function() {
+    var tempDir = helper.tempDir();
     var generator = new Baragon.ClientGenerator("Client", schema);
-    expect(generator.render()).to.match(/function Client/);
+    var writePath = tempDir + "/client.js";
+
+    fs.writeFileSync(writePath, generator.render());
+    var context = helper.loadModule(writePath);
+
+    var Client = context.Client;
+    expect(Client).to.be.a('function');
   });
 });
--- test/helper.js
+++ test/helper.js
@@ -1,9 +1,36 @@
 'use strict';
 
+var temp = require("temp");
+var vm = require("vm");
+var fs = require("fs");
+
 var Helper = {
   loadSchema: function(path) {
-    var fs = require("fs");
     return JSON.parse(fs.readFileSync(path));
+  },
+
+  tempDir: function() {
+    return temp.mkdirSync();
+  },
+
+  loadModule: function(filePath, baragon) {
+    var dependencies = {
+      baragon: require("../lib/baragon")
+    };
+    var exports = {};
+    var context = {
+      require: function(name) {
+        return dependencies[name];
+      },
+      exports: exports,
+      console: console,
+      module: {
+        exports: exports
+      }
+    };
+
+    vm.runInNewContext(fs.readFileSync(filePath), context);
+    return context;
   }
 }
 
